18c149ebe858b53d654f36c644ad048e527c8f7f
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2020-02-21 16:33:59 +0200

    Upd: loader color randomizer

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import './Loader.scss'
+import classes from './Loader.module.scss'
 
 export default function Loader() {
+   const { loader, wrapper, square } = classes
+
    const loaderCols = []
    for (let i = 0; i < 4; i++) {
       loaderCols.push(
-         <div className="l_square" key={i}>
+         <div className={square} key={i}>
             <span/><span/><span/>
          </div>
       )
    }
 
    return (
-      <div className="loader">
-         <div>
+      <div className={loader}>
+         <div className={wrapper}>
             {loaderCols}
          </div>
       </div>
